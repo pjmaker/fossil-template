067d2391afdf1238a115b07fda1d0d2aa6cc5ab0
pjmaker <philip.maker@gmail.com>
2015-01-04 18:36:17 +0930

    Added an example name check in TCL

---
+++ example-name-ok.tcl
@@ -0,0 +1,43 @@
+#! /usr/bin/tclsh
+#
+# example-name-ok.tcl - an example naming convention checkerer
+#
+# The method used:
+# 
+# split a name using the separators split
+# then match each part against a series of regexp patterns in match
+#
+
+set split {-}
+set match {
+  {P90}
+  {WP[0-9]+}
+  {FS CD TP TUT}
+}
+
+set name [lindex $argv 0]
+set parts [split $name $::options(-split)]
+
+if {[llength $parts] != [llength $::options(-match)]} {
+  puts "# of parts mismatch!"
+  exit 1
+}
+
+set i 0
+foreach f $::options(-match) { 
+  set ok 0
+  foreach pat [lindex $::options(-match) $i] {
+    if {[regexp $pat [lindex $parts $i]]} { 
+      set ok 1
+      break
+    }
+    incr i
+  }
+  if {!$ok}} {
+    exit 102
+  }
+}
+
+exit 0 ;# success
+
+
